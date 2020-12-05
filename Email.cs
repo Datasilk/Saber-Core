e37cea55b39c6585282ba982a4d749248c67d713
Mark Entingh <entingh@gmail.com>
2020-12-05 05:46:51 -0800

    updated email send method to use a Func argument for RFC2822 so it is only generated if the developer explicitly needs it

--- Email.cs
+++ Email.cs
@@ -14,8 +14,8 @@ namespace Saber.Core
         /// Send an email using the preferred email client based on the type of email being sent
         /// </summary>
         /// <param name="message">The message being sent</param>
-        /// <param name="type">The type of message to send. Known types are "signup", "forgotpass", and "newsletter", 
-        /// but vendor plugins can contain custom message types.</param>
+        /// <param name="type">The type of message to send. Known types are "signup" and "forgotpass", 
+        /// but vendor plugins can contain custom message types by using Vendor.IVendorEmails.</param>
         public static void Send(MailMessage message, string type)
         {
             Handle(message, type);
