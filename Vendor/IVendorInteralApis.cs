41adf48660c5b9b8ec587df56648b78f7002613e
Mark Entingh <entingh@gmail.com>
2021-11-17 01:00:28 -0600

    added Internal API system

---
+++ Vendor/IVendorInteralApis.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Saber.Vendor
+{
+    public interface IVendorInteralApis
+    {
+        public List<InternalApi> Apis { get; set; }
+    }
+}
---
+++ Vendor/InternalApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Saber.Core;
+
+namespace Saber.Vendor
+{
+    public class InternalApi
+    {
+        public string Key { get; set; }
+        public Func<IRequest, Dictionary<string, object>, string> Method { get; set; }
+
+        public static string Call(string key, IRequest request, Dictionary<string, object> data)
+        {
+            var api = Vendors.InternalApis.Where(a => a.Key == key).FirstOrDefault();
+            if(api != null)
+            {
+                return api.Method(request, data);
+            }
+            return "";
+        }
+    }
+}
