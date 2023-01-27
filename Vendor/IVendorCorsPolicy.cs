71365ebfef0588ee102dbd2bb981d25ee8dd71c5
Mark Entingh <entingh@gmail.com>
2023-01-27 09:22:31 -0600

    added AddCorsOptions

--- Vendor/IVendorCorsPolicy.cs
+++ Vendor/IVendorCorsPolicy.cs
@@ -8,6 +8,12 @@ namespace Saber.Vendor
     public interface IVendorCorsPolicy
     {
         /// <summary>
+        /// Add CORS options when configuring services
+        /// </summary>
+        /// <param name="builder"></param>
+        void AddCorsOptions(CorsOptions options);
+
+        /// <summary>
         /// Define CORS policies for cross-domain communication
         /// </summary>
         /// <param name="builder"></param>
