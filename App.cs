5f9a3b0ea3f80723630194de622ec0330ada5986
Mark Entingh <entingh@gmail.com>
2023-01-29 18:39:56 -0600

    changed App.Host to string[]

--- App.cs
+++ App.cs
@@ -14,7 +14,7 @@ namespace Saber
     {
         public static Environment Environment { get; set; } = Environment.development;
         public static string Name { get; set; }
-        public static string Host { get; set; }
+        public static string[] Host { get; set; }
         public static bool IsDocker { get; set; }
         public static Dictionary<string, string> Languages { get; set; }
         public static string[] ServicePaths { get; set; }
